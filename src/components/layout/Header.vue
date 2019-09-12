6642b5d3664a445643efc18e62522770533a705b
NathanEscandor <nathan.escandor@gmail.com>
2019-09-12 16:32:54 -0600

    Updated about page and did some cleanup

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="header">
-    <h1>Planets Vue-er</h1>
+    <h1>Planets.nu Vue-er</h1>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
     <h1>About</h1>
-    <p>This is an about page to test the routing</p>
+    <p>Nathan Escandor - vue demo for i&a recruitment</p>
+    <p>The home page shows a list of games from Planets.nu with status of 'Joining' (in api, used status=1).</p>
+    <p>The data is displayed using a reusable vue component that takes in the api options as a bound data parameter.</p>
+    <p>Manually navigating to #/all_games shows the entire list of games using the same component as the home page</p>
   </div>
 </template>
