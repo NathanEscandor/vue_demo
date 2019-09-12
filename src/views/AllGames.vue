10d96a90a957608ef3aadb7276532e847805c9b1
NathanEscandor <nathan.escandor@gmail.com>
2019-09-12 16:24:46 -0600

    Added a testing page under all_games route

---
+++ src/views/AllGames.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <p>
+      This is a list of all games in the planets.nu list. Used for testing table fields that lack variety in the home page (eg Created By and Password)
+    </p>
+    <GameTable options=""/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import GameTable from '../components/GameTable';
+
+export default {
+  name: 'Home',
+  components: {
+    GameTable
+  }
+}
+</script>
+
+<style>
+</style>
