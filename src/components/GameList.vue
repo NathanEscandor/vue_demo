e6fb7a952d7aed1ccfcb04a45eae7500bd5602c2
NathanEscandor <nathan.escandor@gmail.com>
2019-09-12 13:38:33 -0600

    Added axios for api request and started making game and list structure

---
+++ src/components/GameList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    {{ games }}
+  </div>
+</template>
+
+<script>
+import Game from './Game.vue';
+import axios from 'axios';
+
+export default {
+  name: "GameList",
+  components: {
+    Game
+  },
+  props: ["games"],
+  data() {
+    return {
+      games: null
+    }
+  },
+  mounted() {
+    axios.get('http://api.planets.nu/games/list?status=1')
+      .then(res => (this.games = res))
+      .catch(err => console.log(err));    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
